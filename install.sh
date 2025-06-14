203abf52633607e872103ec1643ffc03fc34289e
Ibrahim Kamel <eng.ibrahimkamel@icloud.com>
2025-06-14 22:36:28 +0300

    workingggggg with separate run folders.

--- install.sh
+++ install.sh
@@ -100,7 +100,7 @@ log_info "Creating ${CONDA_ENV_NAME} environment"
 
 # Install Nextflow in the environment
 log_info "Installing Nextflow and required dependencies in the ${CONDA_ENV_NAME} environment"
-"${MINICONDA_SETUP_DIR}/bin/conda" run -n "${CONDA_ENV_NAME}" conda install -c conda-forge -c bioconda nextflow samtools=1.21 procps -y || {
+"${MINICONDA_SETUP_DIR}/bin/conda" run -n "${CONDA_ENV_NAME}" conda install -c conda-forge -c bioconda nextflow samtools=1.21 procps-ng -y || {
     log_error "Failed to install Nextflow and dependencies"
     exit 1
 }
